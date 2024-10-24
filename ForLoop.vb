e51f8b4fa89726645de42b23c1340c9bbb6f1e84
Asif Ekhlaque <132199879+Asifekhlaque@users.noreply.github.com>
2024-10-24 23:53:07 +0530

    Create ForLoop.vb

---
+++ ForLoop.vb
@@ -0,0 +1,10 @@
+Imports System.Console
+
+Module Program
+    Sub Main(args As String())
+        Dim i As Integer
+        For i = 0 To 5
+            Console.WriteLine(i)
+        Next
+    End Sub
+End Module
